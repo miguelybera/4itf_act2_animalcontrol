9143de6ffdccf2292952b3ae4adc64974f4e9532
miguelybera <josemiguel.ybera.iics@ust.edu.ph>
2022-03-14 21:59:23 +0800

    removed some files and added data file

---
+++ src/custom/data.js
@@ -0,0 +1,13 @@
+export const ANIMALTYPE = {
+    MAMMAL: 'mammal',
+    OVIPAROUS: 'oviparous',
+  };
+  
+  export const ANIMALS = [
+    {
+        name:'',
+        type: '',
+        image:''
+    }
+  ];
+  
