6e411763548c67551ac931f7ac9248366b00ec6b
miguelybera <josemiguel.ybera.iics@ust.edu.ph>
2022-03-22 15:04:59 +0800

    added data

--- src/custom/data.js
+++ src/custom/data.js
@@ -5,29 +5,29 @@ export const ANIMALTYPE = {
   
   export const ANIMALS = [
     {
-        name:'platypus',
+        name:'Bears',
         type: 'mammal',
-        image:'https://cdn4.vectorstock.com/i/1000x1000/75/33/flat-style-character-avatar-icon-vector-19367533.jpg'
+        image:'https://upload.wikimedia.org/wikipedia/commons/7/79/2010-brown-bear.jpg'
     },
     {
-      name:'platypus1',
+      name:'Dears',
       type: 'mammal',
-      image:'https://cdn4.vectorstock.com/i/1000x1000/75/33/flat-style-character-avatar-icon-vector-19367533.jpg'
+      image:'https://media.npr.org/assets/img/2021/11/10/white-tailed-deer-1-ac07593f0b38e66ffac9178fb0c787ca75baea3d.jpg'
   },
   {
-    name:'platypus2',
+    name:'Monkeys',
     type: 'mammal',
-    image:'https://cdn4.vectorstock.com/i/1000x1000/75/33/flat-style-character-avatar-icon-vector-19367533.jpg'
+    image:'https://images.firstpost.com/wp-content/uploads/2019/05/Vervet_Monkey_Chlorocebus_pygerythrus.jpg'
 },
 {
-  name:'platypus3',
-  type: 'mammal',
-  image:'https://cdn4.vectorstock.com/i/1000x1000/75/33/flat-style-character-avatar-icon-vector-19367533.jpg'
+  name:'Chickens',
+  type: 'oviparous',
+  image:'https://cdn.britannica.com/18/137318-050-29F7072E/rooster-Rhode-Island-Red-roosters-chicken-domestication.jpg?w=400&h=300&c=crop'
 },
 {
-  name:'platypus4',
-  type: 'mammal',
-  image:'https://cdn4.vectorstock.com/i/1000x1000/75/33/flat-style-character-avatar-icon-vector-19367533.jpg'
+  name:'Ducks',
+  type: 'oviparous',
+  image:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/how-to-keep-ducks-call-ducks-1615457181.jpg?resize=640:*'
 },
   ];
   
